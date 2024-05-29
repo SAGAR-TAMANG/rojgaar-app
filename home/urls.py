b59a2b2b5021eba59b9e5d32bb6731a9f275b420
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-05-30 00:00:17 +0530

    added static

--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from ..main import settings
 from django.urls import path
 from .views import index, about, why, services, intro, assetlinks_json
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('why', why),
 
     path('.well-known/assetlinks.json', assetlinks_json, name='assetlinks_json'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
